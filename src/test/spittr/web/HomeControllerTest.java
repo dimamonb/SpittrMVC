4471f2e14f7203f8046c7ee7aa1ed79680fd9eef
d.onatskiy <dimaon@gmail.com>
2019-04-01 17:26:28 +0300

    Initial commit

---
+++ src/test/spittr/web/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package spittr.web;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class HomeControllerTest {
+
+    @Test
+    public void home() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+
+}
